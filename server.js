1daf5fc6351474a3a3048e3f390849e47ab204aa
JaylanChen <jinliang.chen@outlook.com>
2019-03-09 21:13:06 +0800

    add express

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-console.log('server');
+const express = require('express');
+
+// Create global app object
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+// finally, let's start our server...
+var server = app.listen(9386, function () {
+    var addr = server.address();
+    var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    console.log("Express server listening on " + bind);
+});
+server.on("error", onError);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== "listen") {
+      throw error;
+    }  
+    var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
