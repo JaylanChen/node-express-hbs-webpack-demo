068620539119d08937bdc62c87c0b203bad4bb17
Jaylan Chen <chenjinl@mail.open.com.cn>
2019-05-05 17:50:46 +0800

    edit babel config

--- babel.config.js
+++ babel.config.js
@@ -2,21 +2,18 @@ module.exports = function (api) {
     api.assertVersion('^7.4');
     api.cache(true);
     return {
-        'presets': [
+        presets: [
             ['@babel/preset-env', {
-                'modules': false,
-                'targets': {
-                    'browsers': ['> 1%', 'last 10 versions', 'not ie <= 9']
+                modules: false,
+                targets: {
+                    browsers: ['> 1%', 'last 10 versions', 'not ie <= 9']
                 },
-                'useBuiltIns': 'usage',
-                'corejs': { 'version': 3, proposals: true }
+                useBuiltIns: 'entry', // usage
+                corejs: 2
             }]
         ],
-        'plugins': [
-            ['@babel/plugin-proposal-decorators', { "legacy": true }],
-            ['@babel/plugin-proposal-class-properties', { "loose": true }],
-            '@babel/plugin-proposal-export-default-from',
-            ['@babel/plugin-transform-runtime', { 'corejs': 3, "helpers": true, "regenerator": false, "useESModules": true, }]
+        plugins: [
+            // ['@babel/plugin-transform-runtime', { corejs: 2, helpers: true, regenerator: false, useESModules: true, }]
         ]
     };
 };
--- client/js/common.js
+++ client/js/common.js
@@ -1,4 +1,5 @@
 import "../css/style.css";
+require('core-js');
 
 // const $ = require("jquery")
 // // jquery
